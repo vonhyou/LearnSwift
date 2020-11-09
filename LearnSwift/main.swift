9da93a43c291ea6e4de15bee9688ae6778dbf404
vonhyou <vonhyou@yeah.net>
2020-11-09 22:35:24 +0800

    practice print

--- LearnSwift/main.swift
+++ LearnSwift/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+// 如果不想换行
+print("Hello, World!", terminator: "")
+
+// 声明常量
+// 并卵用的中文变量名
+let maxNumber  = 10, 你好 = "你好，世界！"
+
+//声明变量
+var a = 1, b = 3, c = 5
+
+print(maxNumber, 你好)
+
+print(a, b, c)
 
