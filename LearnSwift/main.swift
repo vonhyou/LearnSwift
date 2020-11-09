272c5cf1729cb6bf9c7919f2a08583e6470c723e
vonhyou <vonhyou@yeah.net>
2020-11-09 22:36:34 +0800

    没用的东西增加了

--- LearnSwift/main.swift
+++ LearnSwift/main.swift
@@ -12,12 +12,12 @@ print("Hello, World!", terminator: "")
 
 // 声明常量
 // 并卵用的中文变量名
-let maxNumber  = 10, 你好 = "你好，世界！"
+let maxNumber  = 10, 你好 = "你好，世界！", 👀 = "👀"
 
 //声明变量
 var a = 1, b = 3, c = 5
 
-print(maxNumber, 你好)
+print(maxNumber, 你好, 👀)
 
 print(a, b, c)
 
